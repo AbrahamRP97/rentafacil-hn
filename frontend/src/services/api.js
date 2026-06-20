7b7fe9f2ad61fdbbb7ef321769870850463b25a5
AbrahamRP97 <abrahamj.rivera97@gmail.com>
2026-06-19 19:55:45 -0600

    conexión del frontend con Railway.

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api'
+  baseURL: 'https://rentafacil-hn-production.up.railway.app/api'
 })
 
 export const getPropiedades = () => api.get('/propiedades')
